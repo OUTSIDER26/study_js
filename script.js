e1704beb814e8d1f36a0bbe31ded0039bc22c81e
Anton Prilepa <zomba496206@yandex.ru>
2021-01-21 16:04:38 +0300

    урок3

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-'use strict';
+let money = 10000,
+    income = 'реклама',
+    addExpenses = 'Интернет, Такси, Комуналка',
+    deposit = true,
+    mission = 20000,
+    period = 2,
+    budgetDay = 10000/30; 
 
-//let question = confirm('тебе есть 18 лет?');
-//console.log(question); 
-//let question2 = prompt('сколько тебе лет?');
-//console.log(question2);
+    console.log(typeof money);
+    console.log(typeof income);
+    console.log(typeof deposit);
 
-let n = 4;
-if (n === 5){
-    console.log('команда 1');
-    console.log('команда 2');
-} else if(n === 4){
-    console.log('команда 3');
-} else {
-    console.log('не верно');
-}
+    console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяца');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log(budgetDay);
+
+
