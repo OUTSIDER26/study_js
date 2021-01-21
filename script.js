8166a17fc58b63b44d286b99e4d83dcbd7f187ff
Anton Prilepa <zomba496206@yandex.ru>
2021-01-21 13:26:42 +0300

    откатил файл скрипт

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-console.log(6);
+let money = 10000,
+    income = 'реклама',
+    addExpenses = 'Интернет, Такси, Комуналка',
+    deposit = true,
+    mission = 20000,
+    period = 2,
+    budgetDay = 10000/30; 
 
+    console.log(typeof money);
+    console.log(typeof income);
+    console.log(typeof deposit);
+
+    console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяца');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log(budgetDay);
 
 
