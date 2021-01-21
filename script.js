2c3b16535f84a0a56789787d57158a67f173b5b7
Anton Prilepa <zomba496206@yandex.ru>
2021-01-21 13:01:11 +0300

    start lesson03

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-let money = 10000,
-    income = 'реклама',
-    addExpenses = 'Интернет, Такси, Комуналка',
-    deposit = true,
-    mission = 20000,
-    period = 2,
-    budgetDay = 10000/30; 
 
-    console.log(typeof money);
-    console.log(typeof income);
-    console.log(typeof deposit);
-
-    console.log(addExpenses.length);
-
-console.log('Период равен ' + period + ' месяца');
-console.log('Цель заработать ' + mission + ' рублей');
-
-console.log(addExpenses.toLowerCase().split(', '));
-
-console.log(budgetDay);
 
 
