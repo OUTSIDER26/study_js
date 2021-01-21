54598df51c81188aed79d4581fb80bf4cd7a1da2
Anton Prilepa <zomba496206@yandex.ru>
2021-01-21 18:38:34 +0300

    Домашнее задание 3

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let money = 10000,
+'use strict';
+
+let money = +prompt('Ваш месячный доход?'),
     income = 'реклама',
-    addExpenses = 'Интернет, Такси, Комуналка',
-    deposit = true,
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'пример: "Квартплата, проездной, кредит'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 20000,
-    period = 2,
-    budgetDay = 10000/30; 
+    period = 2;
+    
+    
 
     console.log(typeof money);
     console.log(typeof income);
@@ -17,6 +20,39 @@ console.log('Цель заработать ' + mission + ' рублей');
 
 console.log(addExpenses.toLowerCase().split(', '));
 
-console.log(budgetDay);
+
+
+let expenses1 = prompt('Введите обязательную статью расходов?'),
+    expenses2 = prompt('Введите обязательную статью расходов?'),
+    amount1 = +prompt('Во сколько это обойдется?'),
+    amount2 = +prompt('Во сколько это обойдется?'),
+    budgetMonth = (money - (amount1 + amount2));
+    console.log(budgetMonth);
+let target1 = (mission / budgetMonth);
+    console.log(Math.ceil(target1));
+let budgetDay = +budgetMonth / 30;
+    console.log(Math.floor(budgetDay));
+    switch (true) {
+        case (+budgetDay > 1200):
+            console.log('У вас высокий уровень дохода');
+            break;
+        case ((+budgetDay > 600) && (+budgetDay < 1200)):
+            console.log('У вас средний уровень дохода');
+            break;
+        case ((+budgetDay < 600) && (+budgetDay > 0)):
+            console.log('К сожалению у вас уровень дохода ниже среднего');
+            break;
+        case (+budgetDay < 0):
+            console.log('Что то пошло не так');
+            break;
+            default:
+                console.log('не работает');       
+
+
+    }
+
+
+
+
 
 
