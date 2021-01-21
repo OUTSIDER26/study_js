ca7235b9774f89d8d45ac4f1c0d7edeacfc989d9
Anton Prilepa <zomba496206@yandex.ru>
2021-01-21 16:01:31 +0300

    урок 3

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+'use strict';
 
+//let question = confirm('тебе есть 18 лет?');
+//console.log(question); 
+//let question2 = prompt('сколько тебе лет?');
+//console.log(question2);
 
-
+let n = 4;
+if (n === 5){
+    console.log('команда 1');
+    console.log('команда 2');
+} else if(n === 4){
+    console.log('команда 3');
+} else {
+    console.log('не верно');
+}
